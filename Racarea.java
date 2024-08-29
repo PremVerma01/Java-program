46f596abca5abd68af5d8de5a591c79f29aa4b48
Premnarayan Verma <premvrm01@gmail.com>
2024-08-29 15:34:03 +0530

    Add files via upload

---
+++ Racarea.java
@@ -0,0 +1,11 @@
+public class Racarea
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println("Enter parameters for cube");
+		int num1=Integer.parseInt(args[0]);
+		int num2=Integer.parseInt(args[1]);
+		int area=num1*num2;
+		System.out.println("area of ractangle is"+area);
+	}
+}
